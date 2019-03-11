b3e6da2fbf2e38f837dcdc41063c5143c6f48b37
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-03-11 19:42:16 +0000

    Sketching out a Tile class

---
+++ src/utils/tile.ts
@@ -0,0 +1,50 @@
+type Opts = {
+    name: string;
+};
+
+export class Tile {
+    private _name: string;
+    private _id: string;
+    private _px: number[];
+    private _len: number;
+    constructor(opts: Opts) {
+        this._name = opts.name || 'new tile';
+        this._id = this.makeId();
+        this._len = 64;
+        this._px = this.makePx(this._len);
+    }
+    public updatePx(idx: number, value: number) {
+        if (this.validIdx(idx) && this.validValue(value)) {
+            this.px[idx] = value;
+        }
+    }
+    private makeId(): string {
+        return 'this is the id';
+    }
+    private makePx(len: number): number[] {
+        const px = [];
+        for (let i = 0; i < len; i++) {
+            px.push(0);
+        }
+        return px;
+    }
+    get px(): number[] {
+        return this._px;
+    }
+    set px(newPx: number[]) {
+        this._px = newPx;
+    }
+    get name(): string {
+        return this._name;
+    }
+    get id(): string {
+        return this._id;
+    }
+    private validIdx(idx: number): boolean {
+        return Number.isInteger(idx) && idx >= 0 && idx < this._len;
+    }
+    private validValue(value: number): boolean {
+        return Number.isInteger(value) && value >= 0 && value < 16;
+    }
+
+}
---
+++ tests/unit/tile.spec.ts
@@ -0,0 +1,49 @@
+import { Tile } from '@/utils/tile.ts';
+
+describe('Tile', () => {
+  it('instantiates', () => {
+    const tile = new Tile({
+      name: '',
+    });
+    expect(tile).toBeInstanceOf(Tile);
+  });
+  it('takes a name from passed options', () => {
+    const tile = new Tile({name : 'a unique tile'});
+    expect(tile.name).toBe('a unique tile');
+  });
+  it('gets an id', () => {
+    const tile = new Tile({name: ''});
+    expect(tile.id).toBeTruthy();
+  });
+  it('has a pixel array of the correct length of 0s', () => {
+    const tile = new Tile({name: ''});
+    expect(tile.px).toEqual([0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0]);
+  });
+  it('updates valid pixel data', () => {
+    const tile = new Tile({name: ''});
+    tile.updatePx(3, 12);
+    expect(tile.px).toEqual([0, 0, 0, 12, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0]);
+  });
+  it('rejects invalid pixel data', () => {
+    const tile = new Tile({name: ''});
+    tile.updatePx(-1, 12);
+    tile.updatePx(65, 12);
+    tile.updatePx(5, -3);
+    tile.updatePx(5, 2.4);
+    tile.updatePx(5, -3);
+    tile.updatePx(3.6, -3);
+    expect(tile.px).toEqual([0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0]);
+  });
+});
