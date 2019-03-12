a15bb714e3682e8dfce840665d280705a90ccff2
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-03-12 21:22:27 +0000

    bringing in some default tile data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,45 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Library</router-link> |
+      <router-link to="/tiles">Tiles</router-link> | 
+      <router-link to="/palettes">Palettes</router-link>
     </div>
+    {{ tileData() }}
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { FileDataStore } from './utils/user-data';
+import { Tile } from './utils/tile';
+import { Palette } from './utils/palette';
+@Component({
+  components: {},
+  data() {
+    return {
+      tileData: () => {
+        const defaultTile = new Tile({ name: 'new tile' });
+        const defaultPalette = new Palette({name: 'new palette'});
+        return new FileDataStore({
+          configName: 'app-data',
+          defaults: {
+            tiles: [
+              defaultTile,
+            ],
+            palettes: [
+              defaultPalette,
+            ],
+          },
+        });
+      },
+    };
+  },
+})
+export default class App extends Vue {
+
+}
+</script>
 <style lang="scss">
 </style>
