a5022a2e1d11afa260d510b3375830817a8b9658
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-03-31 10:53:11 +0100

    support for paletesets

---
+++ src/views/PaletteSets.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <h1>Palette Sets</h1>
+      <div class="grid-display">
+          {{ tileData }}
+      </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+
+  },
+  props: ['tileData', 'id', 'getPaletteSet'],
+})
+export default class PaletteSets extends Vue {}
+</script>
