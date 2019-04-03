b48843045f8a2d125a4e57908c766327c1e5a0c5
Sarah Rimron-Soutter <sarah@teaandbiscuits.net>
2019-04-03 18:59:37 +0100

    TAILWIND :D

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store/store';
 import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
+import '@/assets/css/tailwind.css';
 
 
 const requireComponent = require.context(
